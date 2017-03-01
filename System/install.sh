e73d035d7e6a61427a5f12b4d4402147f1666e02
David Ducatel <dducatel@trace.fr>
2017-03-01 14:43:45 +0100

    Correct some speeling #13

--- System/install.sh
+++ System/install.sh
@@ -2,14 +2,16 @@
 
 docker pull tracesoftware/hostmydocs
 
-containerId=$(docker container list -q -f name=hostmysdocs-service)
-if [[ ! -z $containerId ]]
+containerId=$(docker container list -a -q -f name=hostmydocs-service)
+if [ ! -z "$containerId" ]
 then
     docker container stop $containerId
     docker container rm $containerId
 fi
 
-docker run -d -p 80:80 --env-file=.env --name hostmysdocs-service tracesoftware/hostmydocs
+docker run -d -p 80:80 --env-file=.env --name hostmydocs-service tracesoftware/hostmydocs
+docker stop hostmydocs-service
 
-cp -f ./hostmysdocs.service /etc/systemd/system/
-systemctl enable hostmysdocs.service
+cp -f ./hostmydocs.service /etc/systemd/system/
+systemctl enable hostmydocs.service
+systemctl start hostmydocs.service

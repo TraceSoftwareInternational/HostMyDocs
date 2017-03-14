9ac1c30a3fba5ad8af3f84cdd1264cb57e7f2e28
Matthieu Vion <mvion@trace-software.com>
2017-03-14 09:08:19 +0100

    Closes #16

--- BackEnd/src/middleware.php
+++ BackEnd/src/middleware.php
@@ -3,6 +3,8 @@
 use Slim\Middleware\HttpBasicAuthentication;
 
 $slim->add(new HttpBasicAuthentication([
+    'relaxed' => [],
     'path' => '/addProject',
+    'secure' => $slim->getContainer()->get('shouldSecure'),
     'users' => $slim->getContainer()->get('authorizedUser')
 ]));

c1d2c4ffedc11c6e7153a6a44a7911946979b303
Jonathan Selle <jonatjano@gmail.com>
2018-06-04 14:55:14 +0200

    container provide a projectController

--- BackEnd/src/settings.php
+++ BackEnd/src/settings.php
@@ -1,36 +1,39 @@
 <?php
 
+$storageRoot = '/var/www/html/data/docs';
+$archiveRoot = '/var/www/html/data/archives';
+
 return [
-    'settings' => [
-        'displayErrorDetails' => true,
-    ],
+	'settings' => [
+		'displayErrorDetails' => true,
+	],
 
-    'storageRoot' => '/var/www/html/data/docs',
-    'archiveRoot' => '/var/www/html/data/archives',
-    'shouldSecure' => function () {
-        if (getenv('SHOULD_SECURE') === false) {
-            return true;
-        } else {
-            return false;
-        }
-    },
-    'authorizedUser' => function () {
-        $credentials = getenv('CREDENTIALS');
+	'storageRoot' => function () use ($storageRoot) {
+		return $storageRoot;
+	},
+	'archiveRoot' => function () use ($archiveRoot) {
+		return $archiveRoot;
+	},
+	'shouldSecure' => function () {
+		return !getenv('SHOULD_SECURE');
+	},
+	'authorizedUser' => function () {
+		$credentials = getenv('CREDENTIALS');
 
-        if ($credentials !== false) {
-            $splittedCredentials = explode(':', $credentials);
+		if ($credentials !== false) {
+			$splittedCredentials = explode(':', $credentials);
 
-            return [
-                $splittedCredentials[0] => $splittedCredentials[1]
-            ];
-        }
+			return [
+				$splittedCredentials[0] => $splittedCredentials[1]
+			];
+		}
 
-        return [];
-    },
-    'cache' => function () {
-        return new \Slim\HttpCache\CacheProvider();
-    },
-    'projectController' => function () {
-        return new \HostMyDocs\Controllers\ProjectController();
-    }
+		return [];
+	},
+	'cache' => function () {
+		return new \Slim\HttpCache\CacheProvider();
+	},
+	'projectController' => function () use ($storageRoot, $archiveRoot) {
+		return new \HostMyDocs\Controllers\ProjectController($storageRoot, $archiveRoot);
+	}
 ];
